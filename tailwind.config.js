9a0f61a16daf5f4affa5e5caea354601ebf0c8f5
Aurelie Nogueira <aurelienogueira@Aurelies-MacBook-Pro.local>
2024-12-15 22:49:47 +0100

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const { urlencoded } = require('express')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.hbs", "./public/scripts/**/*.js"]
 ,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#11121a",
+        "hover-color": "#272832",
+        "accent-color": "#0071FF",
+        "text-color": "#c9c9c9",
+        },
+      
+        backgroundImage: {
+          "bg-open-menu": "url('../images/bg-open-menu.png')",
+          "bg-topo-image": "url('../images/topo.jpeg')"
+        }
+    },
   },
   plugins: [],
 }
